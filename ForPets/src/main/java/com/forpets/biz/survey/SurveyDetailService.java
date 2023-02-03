3d91aaedd953e4d4cbb0293419ecd5971fc72bba
SEOYEON LEE <110285874+Seoyeongit@users.noreply.github.com>
2023-02-03 17:17:23 +0900

    Revert "survey 통합"
    This reverts commit 9000b51f2cfb8fa89385183a50694a24b23ab2fc.

--- ForPets/src/main/java/com/forpets/biz/survey/SurveyDetailService.java
+++ ForPets/src/main/java/com/forpets/biz/survey/SurveyDetailService.java
@@ -1,23 +1,7 @@
 package com.forpets.biz.survey;
-
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 /*
  *  상세설문지관련 서비스는 여기서 추상메서드를 추가하세요.
  */
-@Service
 public interface SurveyDetailService {
 
-
-	// 설문지 작성
-	public void insertSurvey(SurveyDetailVO vo);
-	public void insertServey2(SurveyChoiceVO vo);
-	
-	// 설문지 리스트 불러오기
-	public List<SurveyVO> getSurveyList() throws Exception;
-	
-	// 설문지 상세 보기
-	public SurveyDetailVO getSurveyBoard(String c_code);
 }

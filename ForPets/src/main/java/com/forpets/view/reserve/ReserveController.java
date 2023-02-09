f594561e75fe60ffccbed39b1c65dcbd58809083
Crete666 <121783191+Crete666@users.noreply.github.com>
2023-02-10 00:09:26 +0900

    Revert "로그인, reserve Insert 함수 수정"
    This reverts commit 0c251da44922233c58c93c81541c502c9646de0c.

--- ForPets/src/main/java/com/forpets/view/reserve/ReserveController.java
+++ ForPets/src/main/java/com/forpets/view/reserve/ReserveController.java
@@ -97,19 +97,14 @@ public class ReserveController {
 		pvo = (PetVO) session.getAttribute("pet_info");
 		vo = (ReServeVO) session.getAttribute("reserve");
 		String[] pa_List = (String[]) session.getAttribute("pa_List");
-		boolean check = false;
-		if(session.getAttribute("pa_List") != null) check = true;
 		ArrayList<ServiceVO> svoList = (ArrayList) session.getAttribute("servList");
 		int count = svoList.toArray().length;
 		for(int i=0;i<count;i++) {
 			ServiceVO svo = svoList.get(i);
 			System.out.println(i+ "번째 s_num : " + svo.getS_num());
+			System.out.println(1 + "번째 pick_add : " + pa_List[i]);
 			vo.setS_num(svo.getS_num());
-			if(check) {
-				int j = i;
-				System.out.println(i + "번째 pick_add : " + pa_List[j]);
-				vo.setPick_add(pa_List[j]);
-			}
+			vo.setPick_add(pa_List[i]);
 			reserveService.insertReserve(vo, pvo);
 		}
 		System.out.println("---> reserveInsert 완료");

f419a742d0df92668b7abc92093af441f3f99200
Doeun <id5eun30@gmail.com>
2023-02-16 19:06:52 +0900

    23-02-16

--- ForPets/src/main/java/com/forpets/view/admin/AdminController.java
+++ ForPets/src/main/java/com/forpets/view/admin/AdminController.java
@@ -5,12 +5,18 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.forpets.biz.admin.AdminService;
 import com.forpets.biz.admin.AdminVO;
 import com.forpets.biz.admin.impl.AdminDAO;
+import com.forpets.biz.partner.PartnerService;
+import com.forpets.biz.partner.PartnerVO;
+import com.forpets.biz.partner.impl.PartnerDAO;
+import com.forpets.biz.user.UserVO;
+import com.forpets.biz.user.impl.UserDAO;
 
 @Controller
 @RequestMapping("/Admin")
@@ -18,9 +24,9 @@ public class AdminController {
 
 	@Autowired
 	private AdminService admService;
-
+	
 	// 관리자 로그인
-	@RequestMapping(value = "/mgmt", method = RequestMethod.POST)
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(AdminVO vo, HttpServletRequest rq) throws Exception {
 		HttpSession session = rq.getSession();
 
@@ -33,9 +39,16 @@ public class AdminController {
 			return "redirect:/adminLogin.jsp";
 		}
 	}
+	
+	// 관리자 메인 이동
+	@RequestMapping(value="/main")
+	public String adminMain(AdminVO vo) {
+		System.out.println("---> 관리자 메인 이동");
+		return "/Admin/mgmt";
+	}
 
 	// 관리자 정보 보기
-	@RequestMapping(value = "/adminInfo")
+	@RequestMapping(value = "/adminInfo/{adm_id}")
 	public String adminInfo(AdminVO vo, AdminDAO adminDAO, HttpServletRequest request) throws Exception {
 		System.out.println("---> adminInfo 이동");
 		
@@ -47,6 +60,26 @@ public class AdminController {
 		}
 		return "/Admin/adminInfo";
 	}
+	
+	// 회원 관리
+	@RequestMapping(value = "/mgmtUser")
+	public String getUserList(UserVO uvo, UserDAO udao, Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		
+		model.addAttribute("getUser", admService.getUserList(uvo));
+		System.out.println("getUserList");
+		return "/Admin/mgmtUser";
+	}
+	
+	// 파트너 관리
+	@RequestMapping(value="/mgmtPartner")
+	public String getPartList(PartnerVO pvo, PartnerDAO pdao, Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		
+		model.addAttribute("getPartner", admService.getPartList(pvo));	// Model 정보 저장
+		System.out.println("getPartnerList");
+		return "/Admin/mgmtPartner";
+	}
 
 	// 페이지 이동
 	@RequestMapping(value = "/mgmtBoard")
@@ -55,16 +88,16 @@ public class AdminController {
 		return "/Admin/mgmtBoard";
 	}
 
-	@RequestMapping(value = "/mgmtPartner")
-	public String mgmtPartner() throws Exception {
-//		System.out.println("---> mgmtPartner 이동");
-		return "/Admin/mgmtPartner";
-	}
+//	@RequestMapping(value = "/mgmtPartner")
+//	public String mgmtPartner() throws Exception {
+////		System.out.println("---> mgmtPartner 이동");
+//		return "/Admin/mgmtPartner";
+//	}
 
-	@RequestMapping(value = "/mgmtUser")
-	public String mgmtUser() throws Exception {
-//		System.out.println("---> mgmtUser 이동");
-		return "/Admin/mgmtUser";
-	}
+//	@RequestMapping(value = "/mgmtUser")
+//	public String mgmtUser() throws Exception {
+////		System.out.println("---> mgmtUser 이동");
+//		return "/Admin/mgmtUser";
+//	}
 
 }
